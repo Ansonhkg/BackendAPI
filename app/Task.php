b02937be12fb8069f61456ff33397a9f22a11966
Anson <ansonox@gmail.com>
2017-03-31 23:01:59 +0100

    Add Controller

--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     //
+    //App\Task::incomplete();
+    // public static function incomplete(){
+    //    return static::where('completed', 0)->get();
+    // }
+
+    // App\Task::incomplete()->get();
+    public function scopeIncomplete($query){
+        return $query->where('completed', 0);
+    }
+
+    
 }
